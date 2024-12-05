0d846dbfb191a57ab097b7533436f76b19f367e4
HsinYi <11050034@me.mcu.edu.tw>
2024-12-05 18:16:46 +0800

    feat(main): 完成未登入時主頁實作

---
+++ sql_final-main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ClassicModels 管理系統</title>
+        <link rel="stylesheet" href="sql_final-main.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1>ClassicModels 管理系統</h1>
+        </header>
+
+        <div class="home-page">
+            <p class="w1">歡迎使用 ClassicModels 管理系統</p>
+            <p class="w2">本系統僅提供已註冊用戶使用。請登入或註冊以使用完整功能。</p>
+            <div>
+                <button onclick="window.location.href='login.php';">登入</button>
+                <button onclick="window.location.href='register.php';">註冊</button>
+            </div>
+        </div>
+        
+        <footer>@ 2024 ClassicModels. All rights reserved.</footer>
+    </body>
+</html>
