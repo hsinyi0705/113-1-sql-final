2453e0cda9ecda999422dcae1bbce2176c170872
HsinYi <11050034@me.mcu.edu.tw>
2024-12-05 21:22:51 +0800

    feat(order_manage): 訂單管理頁面實作

---
+++ order_manage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ClassicModels 管理系統</title>
+        <link rel="stylesheet" href="manage.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1>訂單管理</h1>
+            <div class="left">
+                <button onclick="window.location.href='success_login_page.php';">返回主頁</button>
+            </div>
+        </header>
+
+        <div class="manage-page">
+            <p class="w1">訂單清單</p>
+        </div>
+        
+        <footer>@ 2024 ClassicModels. All rights reserved.</footer>
+    </body>
+</html>
