364b9c514fdb082d1a005a6e56d37f0c2a615bc4
HsinYi <11050034@me.mcu.edu.tw>
2024-12-06 15:15:23 +0800

    feat(db): 建立資料庫連線

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "o70500hy";
+    $dbname = "classicmodels";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("MySQL 伺服器連結失敗: " . mysqli_connect_error());
+    }
+
+    // mysqli_close($conn);
+?>
